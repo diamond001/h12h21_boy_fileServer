ab27a38d6ad9539685a6804e0a56a8f8a60077ed
diamond001 <zqwt.012@163.com>
2021-06-29 23:20:59 +0800

    mkdir /root/bee

--- 3nodes.sh
+++ 3nodes.sh
@@ -5,9 +5,11 @@ docker rmi $(docker images -q)
 #mkdir -p /root/swarm/bee/node-{1,2,3}
 #mkdir -p /root/swarm/clef/clef-{1,2,3}
 #chmod 777 -R /root/swarm
-mkdir -p /root/swarm/config
-cd /root/swarm/config
+mkdir -p /root/bee
+cd /root/bee
 wget -c https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/docker-compose.yml
 docker-compose up -d
 chmod 777 -R /root/swarm
 docker-compose restart
+sleep 5
+tree -a /root/swarm
