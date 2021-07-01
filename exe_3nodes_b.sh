af2d9d91abc5733e39481941d9b18429a3cada36
diamond001 <zqwt.012@163.com>
2021-07-02 00:03:19 +0800

    exe 3 b sh

--- exe_3nodes_b.sh
+++ exe_3nodes_b.sh
@@ -9,5 +9,5 @@
 # scp -r .env root@192.168.0.129:/root/bee/
 # scp -r .env root@192.168.0.132:/root/bee/
 ssh root@192.168.0.132 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/3nodes_b.sh -O 3nodes_b.sh;
-                        chmod +x 3nodes.sh; 
-			./3nodes_b.sh"
+                        chmod +x 3nodes_b.sh; 
+ 			./3nodes_b.sh"
