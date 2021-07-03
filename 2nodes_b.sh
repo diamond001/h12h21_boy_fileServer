f13aeb37c83e5903a54cd6b11ad181aeb19c8369
diamond001 <zqwt.012@163.com>
2021-07-03 12:41:41 +0800

    2nodes b

--- 2nodes_b.sh
+++ 2nodes_b.sh
@@ -4,9 +4,9 @@ wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/doc
 chmod +x docker-compose.sh
 ./docker-compose.sh
 ################
-#docker stop $(docker ps -aq)
-#docker rm $(docker ps -aq)
-#docker rmi $(docker images -q)
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 mkdir -p /root/swarm/bee/node-{1,2}
 mkdir -p /root/swarm/clef/clef-{1,2}
 chmod 777 -R /root/swarm
@@ -15,5 +15,5 @@ cd /root/bee
 wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.yml -O docker-compose.yml
 docker-compose up -d
 docker-compose restart
-apt install tree
+apt-get install tree
 tree -a /root/swarm
