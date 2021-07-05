858b210be5f003a201dad7463bb7a308288842ed
diamond001 <zqwt.012@163.com>
2021-07-05 20:34:20 +0800

    2nodes b-10

--- exe_2nodes_b.sh
+++ exe_2nodes_b.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#ssh root@192.168.0.143 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
-#                        chmod +x 2nodes_b.sh; 
-#			./2nodes_b.sh"
+ssh root@192.168.0.143 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
+                        chmod +x 2nodes_b.sh; 
+			./2nodes_b.sh"
 
-#ssh root@192.168.0.140 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
-#                        chmod +x 2nodes_b.sh; 
-#			./2nodes_b.sh"
+ssh root@192.168.0.140 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
+                        chmod +x 2nodes_b.sh; 
+			./2nodes_b.sh"
 
-#ssh root@192.168.0.142 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
-#                        chmod +x 2nodes_b.sh; 
-#			./2nodes_b.sh"
+ssh root@192.168.0.142 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
+                        chmod +x 2nodes_b.sh; 
+			./2nodes_b.sh"
 
 ssh root@192.168.0.138 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
                         chmod +x 2nodes_b.sh; 
@@ -27,10 +27,10 @@ ssh root@192.168.0.148 "wget https://raw.githubusercontent.com/diamond001/h12h21
                         chmod +x 2nodes_b.sh; 
 			./2nodes_b.sh"
 
-ssh root@192.168.0.143 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
+ssh root@192.168.0.146 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
                         chmod +x 2nodes_b.sh; 
 			./2nodes_b.sh"
 
-ssh root@192.168.0.146 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
+ssh root@192.168.0.147 "wget https://raw.githubusercontent.com/diamond001/h12h21_boy_fileServer/main/2nodes_b.sh -O 2nodes_b.sh;
                         chmod +x 2nodes_b.sh; 
 			./2nodes_b.sh"
